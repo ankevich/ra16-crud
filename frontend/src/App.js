3a065d16f3e706409777139981f1ec136f238a30
Annie <overheads165@gmail.com>
2023-02-08 13:26:30 +0100

    notes layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+
+const Note = styled.div`
+border: 1px solid black;
+padding: 10px;
+margin: 10px;
+max-width: 300px;
+`;
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -14,15 +22,18 @@ function App() {
 
   return (
     <>
-      {notes.map((note) => {
-        return (
-          <div key={note.id}>
-            <p>{note.content}</p>
-          </div>
-        );
-      })}
+      <h1>Notes</h1>
+      <button onClick={() => {}}>🔄</button>
+
+      {notes.map((note) => (
+        <Note key = {note.id}>
+          <p>{note.content}</p>
+        </Note>
+      ))}
     </>
   );
 }
+
+
 
 export default App;
